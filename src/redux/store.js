6c5ca825a6ff1b3acea1b32112e231fee86ec154
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-07-14 12:30:14 +0530

    s

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { CounterSclice } from './slices/CounterSlice'
-
-export const store = configureStore({
-  reducer: { 
-    counter: CounterSclice
-  },
-})
-
